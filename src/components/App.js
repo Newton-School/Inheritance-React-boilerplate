1b06243d7438c47c04f156f08166784df8d23574
Nightrider0098 <hitesh08011999@gmail.com>
2020-10-01 17:56:06 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+
+        return(
+            <div>
+               //do not write anything here
+            </div>
+        )
+    }
+}
+
+
+export default App;
