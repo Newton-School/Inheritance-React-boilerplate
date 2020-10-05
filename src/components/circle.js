94f68f5e106fa9c48fb0aeef1c7dc57a5580c3a0
Bhimashankar <bhimamalbhage@hotmail.com>
2020-10-05 23:28:25 +0530

    first commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,7 +1,16 @@
-// import 
-
+// import
+import Shape from "./shape";
 // declare class
+class Circle extends Shape {
+  constructor(color, shape, radius) {
+    super(color, shape);
+    this.radius = radius;
+  }
 
+  calculateArea() {
+    return Math.PI * this.radius * this.radius;
+  }
+}
 
 // export
-
+export default Circle;
