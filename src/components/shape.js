899b2b46d4836919aaa89da8a95b42c77d658537
AMER <amer95521@gmail.com>
2020-10-05 12:05:09 +0530

    initial commit

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
+class Shape{
+    constructor(color, shape){
+        this.color = color;
+        this.shape = shape;
+    }
+
+    drawShape() {
+        console.log(`drawing ${this.shape} Shape`);
+    }
+
+    calculateArea() {
+        console.log(`The area of ${this.shape} is : `);
+    }
+}
 
 // export class
+export default Shape;
